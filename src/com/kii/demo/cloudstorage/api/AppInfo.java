d6e70cdcea811e2fafa81fec5cd6d7f21a94d3f2
shean.zhang <shean.zhang@e7cb0379-d053-0e4b-a1ac-43c7dbffadbe>
2012-03-30 10:02:48 +0000

    sz: change build
    git-svn-id: https://source.kii.com/servo/platform/sdk-mobile-client/androidV1/KiiCloudStorageAPIDemo@22026 e7cb0379-d053-0e4b-a1ac-43c7dbffadbe

--- src/com/kii/demo/cloudstorage/api/AppInfo.java
+++ src/com/kii/demo/cloudstorage/api/AppInfo.java
@@ -3,6 +3,7 @@ package com.kii.demo.cloudstorage.api;
 public class AppInfo {
     public static final String APP_ID = "b0bdad85";
     public static final String APP_KEY = "5ea669a7a15f352c9c9f56022016ab65";
-    public static final String BAST_URL = "https://api.kii.com/app/api";
+    //set Base url if need
+    public static final String BASE_URL = "https://api.kii.com/app/api"; 
     
 }
